2a58c20c10bc9c2ac4bda72187dd39b453dfb2f6
anonymous0904 <farcauadda@gmail.com>
2025-03-06 20:49:25 +0200

    working on LSTM

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -37,3 +37,18 @@ class DatabaseHandler:
     def close_connection(self):
         self.connection.close()
         print("Disconnected from the database.")
+
+    # missing values can appear as None or NaN (not a number)
+    def get_metars_df(self):
+        self.connect_to_database()
+        metars_df = self.execute_query("""SELECT * FROM metars""")
+        self.close_connection()
+        return metars_df
+
+    # missing values can appear as None or NaN (not a number)
+    def get_tafs_df(self):
+        self.connect_to_database()
+        # fetch the tafs together with probable phenomena
+        tafs_df = self.execute_query("""SELECT * FROM tafs t, taf_probs tp where t.id = tp.taf_id""")
+        self.close_connection()
+        return tafs_df
