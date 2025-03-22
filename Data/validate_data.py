25f728b68d0a52cb44b057888c4f7f6713e43be8
anonymous0904 <farcauadda@gmail.com>
2025-03-22 16:34:48 +0200

    validate wind_direction

---
+++ Data/validate_data.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import csv_file_handler
+
+
+# calculate the circular mean of a series
+def circular_mean_wind_direction(direction_angles, w=None):
+    if w is None:
+        w = np.ones(len(direction_angles))
+    sin_sum = np.sum(w * np.sin(np.radians(direction_angles)))
+    cos_sum = np.sum(w * np.cos(np.radians(direction_angles)))
+    mean = np.degrees(np.arctan2(sin_sum, cos_sum)) % 360
+    return int(mean)
+
+
+def validate_wind_direction(data_frame):
+    missing_indices = data_frame[data_frame['wind_direction'] == 0].index
+
+    if missing_indices.empty:
+        return data_frame
+
+    wind_direction_to_validate = data_frame['wind_direction'].copy()
+    for i in missing_indices:  # [missing_indices].index:
+        valid_before = data_frame['wind_direction'].loc[:i][data_frame['wind_direction'].loc[:i] != 0].index.tolist()
+        valid_after = data_frame['wind_direction'].loc[i:][data_frame['wind_direction'].loc[i:] != 0].index.tolist()
+
+        if not valid_before or not valid_after:
+            wind_direction_to_validate.loc[i] = 0
+            continue
+
+        idx_before = valid_before[-1]
+        idx_after = valid_after[0]
+
+        dist_before = i - idx_before
+        dist_after = idx_after - i
+        total_dist = dist_before + dist_after
+
+        weight_before = dist_after / total_dist
+        weight_after = dist_before / total_dist
+
+        direction_angles = [data_frame['wind_direction'][idx_before], data_frame['wind_direction'][idx_after]]
+        weights = [weight_before, weight_after]
+        wind_direction_to_validate.loc[i] = circular_mean_wind_direction(direction_angles, weights)
+
+    wind_direction_to_validate.loc[missing_indices[-1]] = wind_direction_to_validate.loc[missing_indices[-1] - 1]
+    data_frame['wind_direction'] = wind_direction_to_validate.astype(int)
+    return data_frame
+
+
+metar_df = csv_file_handler.read_metar_df_from_csv_file()
+print(validate_wind_direction(metar_df))
