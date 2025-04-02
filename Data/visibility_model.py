3f17bbb0115e48faa40648614a598d83e085a1fa
anonymous0904 <farcauadda@gmail.com>
2025-04-02 22:43:51 +0300

    minimal changes in the visibility_model.py but the accuracy stays the same

--- Data/visibility_model.py
+++ Data/visibility_model.py
@@ -2,9 +2,13 @@ import random
 
 import numpy as np
 import pandas as pd
+from keras import Input
+from keras.src.callbacks import EarlyStopping
+from keras.src.losses import Huber
 
+from keras.src.optimizers import Adam
 import csv_file_handler
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 from keras.src.models import Sequential
 from keras.src.layers import Dense, Bidirectional, LSTM, Dropout
 from sklearn.model_selection import train_test_split
@@ -32,10 +36,12 @@ def preprocess_data(df, target_cols, sequence_length=10):
 
 def build_bilstm_model(input_shape):
     model = Sequential()
-    model.add(Bidirectional(LSTM(64, return_sequences=True), input_shape=input_shape))
+    model.add(Input(shape=input_shape))
+    model.add(Bidirectional(LSTM(64, return_sequences=True)))
     model.add(Dropout(0.2))
     model.add(Bidirectional(LSTM(32)))
     model.add(Dense(64, activation='relu'))
+    model.add(Dense(32, activation='relu'))
     model.add(Dense(1))
 
     model.compile(optimizer='adam', loss='mse', metrics=['mae'])
@@ -47,17 +53,18 @@ def predict_visibility_with_bilstm():
     metars_df.index = pd.to_datetime(metars_df['observation_time'], format="%Y-%m-%d %H:%M:%S")
     metars_df = metars_df[['predominant_horizontal_visibility']]
     X, y, scaler = preprocess_data(metars_df, ['predominant_horizontal_visibility'])
-    # split data into train and test information
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     X_train = X_train.astype('float32')
     X_test = X_test.astype('float32')
     y_train = y_train.astype('float32')
     y_test = y_test.astype('float32')
-
     bilstm_model = build_bilstm_model(X_train.shape[1:])
-    bilstm_model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=16)
-
+    early_stopping = EarlyStopping(monitor='val_loss', patience=15, restore_best_weights=True)
+    bilstm_model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=16,
+                     callbacks=[early_stopping])
     bilstm_predictions = bilstm_model.predict(X_test)
+    bilstm_predictions = bilstm_predictions.reshape(-1, 1)
+    y_test = y_test.reshape(-1, 1)
     return scaler.inverse_transform(bilstm_predictions).astype(int), scaler.inverse_transform(y_test).astype(int)
 
 
