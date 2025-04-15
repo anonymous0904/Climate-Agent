4e01d9ad61db2b5cd0a77a9e737b981d98fbca40
anonymous0904 <farcauadda@gmail.com>
2025-04-16 00:35:21 +0300

    Change train test split data and save predictions for wind direction.

--- Data/wind_direction_prediction.py
+++ Data/wind_direction_prediction.py
@@ -3,13 +3,11 @@ import random
 import numpy as np
 import pandas as pd
 from keras.src.callbacks import EarlyStopping
-from matplotlib import pyplot as plt
 
 import csv_file_handler
 from sklearn.preprocessing import MinMaxScaler
 from keras.src.models import Sequential, Model
 from keras.src.layers import Input, Conv1D, MaxPooling1D, Flatten, Dense, Dropout, LSTM, Bidirectional, Reshape
-from sklearn.model_selection import train_test_split
 import tensorflow as tf
 
 seed_value = 42
@@ -18,9 +16,8 @@ np.random.seed(seed_value)
 tf.random.set_seed(seed_value)
 
 
-def evaluate_angular_accuracy(true_angles, pred_angles, thresholds=None):
-    if thresholds is None:
-        thresholds = [5, 10, 20, 30, 45, 60]
+def evaluate_angular_accuracy(true_angles, pred_angles):
+    thresholds = [5, 10, 20, 30, 45, 60]
     angular_diff = np.abs(pred_angles - true_angles)
     angular_error = np.minimum(angular_diff, 360 - angular_diff)
 
@@ -51,7 +48,8 @@ def preprocess_data(df, input_features, sequence_length=24):
         X.append(df_scaled[i:i + sequence_length, :-1])
         y.append(df_scaled[i + sequence_length, -2:])
 
-    return np.array(X), np.array(y), scaler
+    observation_times = df.index[sequence_length:]
+    return np.array(X), np.array(y), scaler, observation_times
 
 
 # BiLSTM - Mean Angular Error: 13.97°
@@ -82,7 +80,7 @@ def preprocess_data(df, input_features, sequence_length=24):
 #     model.compile(optimizer='adam', loss='mse', metrics=['mae'])
 #     return model
 
-# CNN + BiLSTM - Mean Angular Error: 13.83°
+# CNN + BiLSTM - Mean Angular Error: 13.27°
 def build_wind_direction_model(input_shape):
     model = Sequential()
     model.add(Input(shape=input_shape))
@@ -103,20 +101,30 @@ metars_df = csv_file_handler.read_metar_df_from_csv_file()
 metars_df.index = pd.to_datetime(metars_df['observation_time'], format="%Y-%m-%d %H:%M:%S")
 metars_df['hour'] = metars_df.index.hour
 metars_df['month'] = metars_df.index.month
+metars_df['wind_direction'] = metars_df['wind_direction'].rolling(window=3, center=True).mean().bfill().ffill()
 metars_df['wind_dir_sin'] = np.sin(np.deg2rad(metars_df['wind_direction']))
 metars_df['wind_dir_cos'] = np.cos(np.deg2rad(metars_df['wind_direction']))
 input_cols = ['air_temperature', 'dew_point', 'air_pressure', 'hour', 'month', 'wind_dir_sin', 'wind_dir_cos']
 target_cols = ['wind_dir_sin', 'wind_dir_cos']
 
-X, y, scaler = preprocess_data(metars_df, input_cols)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X, y, scaler, observation_times = preprocess_data(metars_df, input_cols)
+
+split_index = int(len(X) * 0.8)
+
+X_train = X[:split_index]
+X_test = X[split_index:]
+y_train = y[:split_index]
+y_test = y[split_index:]
+time_train = observation_times[:split_index]
+time_test = observation_times[split_index:]
+
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 y_train = y_train.astype('float32')
 y_test = y_test.astype('float32')
 
 model = build_wind_direction_model(X_train.shape[1:])
-early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=30, batch_size=32, callbacks=[early_stopping])
 
 predictions = model.predict(X_test)
@@ -132,7 +140,8 @@ print(f"Mean Angular Error: {mean_angular_error:.2f}°")
 angular_error = evaluate_angular_accuracy(actual_direction, pred_direction)
 
 # train_result = pd.DataFrame(
-#     data={'Train Prediction': pred_direction.astype(int),
+#     data={'Time': time_test,
+#           'Train Prediction': pred_direction.astype(int),
 #           'Actual Value': actual_direction.astype(int)})
-# with open('predictions/wind_direction_prediction.txt', 'w') as f:
-#     f.write(train_result.to_string())
+#
+# train_result.to_csv('predictions/wind_direction_prediction.csv', index=False, columns=train_result.columns)
